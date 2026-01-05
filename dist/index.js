3a71d476717c0be43eb9537dbc429aaefb175cf0
Михаил Субботин <alex.ob3233@gmail.com>
2026-01-05 18:40:49 +0500

    Очистка репозитория после исправления .gitignore

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import { db } from "./db";
-import { users } from "./db/schema";
-async function main() {
-    const res = await db.select().from(users);
-    console.log("Users:", res);
-    process.exit(0);
-}
-main().catch((err) => { console.error(err); process.exit(1); });
